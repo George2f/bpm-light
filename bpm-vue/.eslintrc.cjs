07e6f79aada12de136685afed97e3e70d7b5e4da
George2f <juraj.pejnovic@2front.co>
2024-03-16 11:38:38 +0100

    feat(bpm-vue): add initial vue files

---
+++ bpm-vue/.eslintrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+    root: true,
+    env: { browser: true, es2020: true },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:vue/vue3-recommended',
+        'plugin:prettier/recommended',
+    ],
+    ignorePatterns: ['dist', '.eslintrc.cjs'],
+    parser: 'vue-eslint-parser',
+    parserOptions: {
+        parser: '@typescript-eslint/parser',
+        sourceType: 'module',
+    },
+    rules: {},
+};
